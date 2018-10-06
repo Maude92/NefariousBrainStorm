b4da1dd9061a56755a99691281d7c39cd4097184
Flx <felix.leblancv4si@gmail.com>
2018-10-06 16:49:14 -0400

    Bullys should be finish
    test must be conducted

--- BrainStorm_JamOct2018/Assets/TriggerBullys_Droite.cs
+++ BrainStorm_JamOct2018/Assets/TriggerBullys_Droite.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerBullys_Gauche : MonoBehaviour {
+public class TriggerBullys_Droite : MonoBehaviour {
 
 	public GameObject Bully1;
 	public GameObject Bully2;
@@ -16,16 +16,11 @@ public class TriggerBullys_Gauche : MonoBehaviour {
 		animBully2 = Bully2.GetComponent<Animator> ();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.tag == "Player") {
-			animBully1.SetBool ("MoveGauche", true);
-			animBully2.SetBool ("MoveGauche", true);
+			animBully1.SetBool ("MoveDroite", true);
+			animBully2.SetBool ("MoveDroite", true);
 		}
 	}
 }

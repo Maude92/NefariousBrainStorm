cc821eb7924f3d5d1b134ae7bf5874d1a967d065
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-10-06 15:27:27 -0400

    VroomVroom
    Une voiture qui bouge!

--- BrainStorm_JamOct2018/Assets/AuraScript.cs
+++ BrainStorm_JamOct2018/Assets/AuraScript.cs
@@ -33,9 +33,42 @@ public class AuraScript : MonoBehaviour {
 
 	void OnTriggerExit2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
-			controlsplayerscript.maxSpeed = 3f;
+			//controlsplayerscript.maxSpeed = 3f;
+			StartCoroutine (GoBackToNormalSpeed());
 			canBreath = false;
 			animAura.SetBool ("Aura", false);
 		}
+	}
+
+	IEnumerator GoBackToNormalSpeed (){
+		controlsplayerscript.maxSpeed = 0.6f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 0.7f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 0.8f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 0.9f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 1f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 1.2f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 1.4f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 1.6f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 1.8f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 2f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 2.2f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 2.4f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 2.6f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 2.8f;
+		yield return new WaitForSeconds (0.1f);
+		controlsplayerscript.maxSpeed = 3f;
 	}
 }

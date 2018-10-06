8369f8a9fcf7658a3d56793ef6601581d841073e
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-10-05 21:47:52 -0400

    ControlesEtCamera

---
+++ BrainStorm_JamOct2018/Assets/CameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float xMargin = 1f;
+	public float yMargin = 1f;
+	public float xySmooth = 3;
+	public Vector2 maxXY;
+	public Vector2 minXY;
+
+	Transform playerTransform;
+
+	//public Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void LateUpdate(){
+		trackPlayer ();
+	}
+
+	bool CheckXMargin(){
+		return Mathf.Abs (transform.position.x - playerTransform.position.x) > xMargin;
+	}
+
+	bool CheckYMargin(){
+		return Mathf.Abs (transform.position.y - playerTransform.position.y) > yMargin;
+	}
+
+	void trackPlayer(){
+		float targetX = transform.position.x;
+		float targetY = transform.position.y;
+
+		if (CheckXMargin ()) 
+			targetX = Mathf.Lerp (transform.position.x, playerTransform.position.x, xySmooth * Time.deltaTime);
+		
+
+		targetX = Mathf.Clamp (targetX, minXY.x, maxXY.x);
+
+		if (CheckYMargin ()) 
+			targetY = Mathf.Lerp (transform.position.y, playerTransform.position.y, xySmooth * Time.deltaTime);
+		
+		targetY = Mathf.Clamp (targetY, minXY.y, maxXY.y);
+
+
+		transform.position = new Vector3 (targetX, targetY, transform.position.z);
+	}
+}
--- BrainStorm_JamOct2018/Assets/ControlsPlayer.cs
+++ BrainStorm_JamOct2018/Assets/ControlsPlayer.cs
@@ -4,13 +4,68 @@ using UnityEngine;
 
 public class ControlsPlayer : MonoBehaviour {
 
+	Rigidbody2D rbPlayer;
+
+	public float maxSpeed = 3f;
+
 	// Use this for initialization
 	void Start () {
-		
+		rbPlayer = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		UserInputs ();
+	}
+
+	void FixedUpdate (){
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		rbPlayer.velocity = new Vector2 (h * maxSpeed, v * maxSpeed);
+	}
+
+	void UserInputs() {
+
+	// A button
+		if (Input.GetButtonDown ("360_AButton")){
+			print ("Je pèse sur le bouton A");
+		}
+
+	// B button
+		if (Input.GetButtonDown ("360_BButton")){
+			print ("Je pèse sur le bouton B");
+		}
+
+	// X button
+		if (Input.GetButtonDown ("360_XButton")){
+			print ("Je pèse sur le bouton X");
+		}
+
+	// Y button
+		if (Input.GetButtonDown ("360_YButton")){
+			print ("Je pèse sur le bouton Y");
+		}
+
+	// Back button
+		if (Input.GetButtonDown("360_BackButton")){
+			print ("Je pèse sur le bouton Back");
+		}
+
+	// Start button
+		if (Input.GetButtonDown ("360_StartButton")){
+			print ("Je pèse sur Start");
+		}
+
+	// Trigger Left
+		if (Input.GetAxis("360_TriggerL") > 0.001) {
+			print ("Je pèse sur le Trigger Gauche (L) ");
+		}
+
+	// Trigger Right
+		if (Input.GetAxis ("360_TriggerR") > 0.001){
+			print ("Je pèse sur le Trigger Droit (R)");
+		}
+
 	}
 }

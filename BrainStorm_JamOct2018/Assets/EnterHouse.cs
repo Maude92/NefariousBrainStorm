d5252e44313b224e558c024e78b0359343247272
AthenaTyrell <36172699+AthenaTyrell@users.noreply.github.com>
2018-10-07 14:20:21 -0400

    CanvasEnd

---
+++ BrainStorm_JamOct2018/Assets/EnterHouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterHouse : MonoBehaviour {
+	public GameObject Canvas;
+	// Use this for initialization
+	void Start () {
+		Canvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D( Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			Canvas.SetActive (true);
+		}
+	}
+}
